c5b509a8434f4094198225a85cca0610f0217ddf
Bararmna <165829084+Bararmna@users.noreply.github.com>
2024-04-03 13:06:23 -0400

    Add files via upload

---
+++ P1LAB1_BararmnaMissowina.py
@@ -0,0 +1,9 @@
+# Missowina Bararmna
+# 4/3/2024
+# P1LAB1
+# Take input from users.
+
+First_Name= input("Enter you first name:")
+Last_Name= input("Enter you last name:")
+print("Hello,", First_Name, Last_Name,"!" , "Wellecom to CTI-110")
+
