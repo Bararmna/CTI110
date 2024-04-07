88b2aac6cad5c107799562707a382f707a4a3228
Bararmna <165829084+Bararmna@users.noreply.github.com>
2024-04-07 12:31:44 -0400

    P1HW1 changes

---
+++ P1HW1_BararmnaMissowina.py
@@ -0,0 +1,14 @@
+# Bararmna Missowina
+# 4/5/2024
+# P1HW1
+# codint to collect input form users
+
+num1 = int(input("Enter integer:"))
+print(num1)
+print("You entered: 4")
+print(num1,"squared","is",num1*num1)
+print("and",num1,"cubed", "is",num1*num1*num1)
+num2 = int(input("Enter another integer:"))
+print(num2)
+print(num1,"+",num2, "is",num1+num2)
+print(num1,"*",num2, "is",num1*num2)
