aba76239f86a8b93a6991f83977db7ab0c0500fb
Bararmna <165829084+Bararmna@users.noreply.github.com>
2024-04-17 07:49:40 -0400

    P2HW1 submission

---
+++ P2HW1_BararmnaMissowina.py
@@ -0,0 +1,46 @@
+
+# Bararmna Missowina
+# 4/6/2024
+# P1HW2
+# collect frmm users.
+
+
+# Pseudocod
+#3. Ask user to enter their budget
+
+#4. Ask user to enter travel destination
+
+#5. Ask user for amount they will spend on gas
+
+#6. Ask user for amount they will spend on accommodation
+
+#7. Ask user for amount they will spend on food
+
+#8. Add expenses
+
+#9. Subtract expenses from budget
+
+#10. Display results
+print("This program calculates and displays travel expenses")
+Budget = float(input("Enter Budget:"))
+Destination = input("Enter travel destination:")
+Gaz_expense = float(input("How much do you think you will spend on gaz:"))
+Accomodation_expense = float(input("Approximately, how much will you spend for accomodation:"))
+Food_expense= float(input("How much will you spend for food:"))
+print()
+print("Total_Travel_Expenses")
+print()
+print(f"Location:               {Destination}")
+print(f"Initial Budget:         ${Budget: .2f}")
+print(f"Gaz_expense:            ${Gaz_expense: .2f}")
+print(f"Accomodation_expense:   ${Accomodation_expense: .2f}")
+print(f"Food_expense            ${Food_expense: .2f}")
+                         
+Total_Travel_Expenses = Gaz_expense + Accomodation_expense + Food_expense
+
+#print(f"Total Travel Expenses:  ${Total_Travel_Expenses: .2f}")
+remaining_Budget = Budget - Total_Travel_Expenses
+print()
+print(f"Remaining Budget:       ${remaining_Budget: .2f}")
+
+
